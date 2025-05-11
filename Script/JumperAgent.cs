b95040e9dc1934df0b7341451819ed2aa80b4468
Salih <salih.bouzian@student.ap.be>
2025-05-11 23:57:33 +0200

    Eerste commit

---
+++ Script/JumperAgent.cs
@@ -0,0 +1,105 @@
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+
+public class JumperAgent : Agent
+{
+    public Transform Obstacle;
+    public float ObstacleSpeed;
+    private bool hasJumped = false;
+    private bool NegativeReceived = false;
+    private bool Passed = false;
+    public Rigidbody rb;
+    public Rigidbody obstacleRb;
+
+    public override void OnEpisodeBegin(){
+        ObstacleSpeed = Random.Range(4f, 7f);
+
+
+        this.transform.localPosition = new Vector3(0,0.5f,0);
+        this.Obstacle.localPosition = new Vector3(0,0.5f,9);
+        
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        sensor.AddObservation(Obstacle.localPosition);
+        sensor.AddObservation(this.transform.localPosition);
+    }
+
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+
+        int jumpAction = actionBuffers.DiscreteActions[0];
+
+        if (jumpAction == 1 && !hasJumped)
+        {
+            hasJumped = true;
+            rb.AddForce(Vector3.up * 5f, ForceMode.VelocityChange);
+        } 
+
+        if(hasJumped && !Passed && !NegativeReceived){
+            Debug.Log("Slechte jump -");
+            AddReward(-0.1f);
+            NegativeReceived = true;
+        }
+
+        
+    }
+
+    public void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.CompareTag("Plane"))
+            {
+            //check collision met grond.
+            hasJumped = false;
+            NegativeReceived = false;
+            }
+
+        if(collision.gameObject.CompareTag("Obstacle"))
+        {
+            //check collision met de obstakel
+            AddReward(-1.0f);
+            EndEpisode();
+        }
+        
+    }
+
+    public void onTriggerEnter(Collision collision){
+        if(collision.gameObject.CompareTag("UpObstacle")){
+            Debug.Log("Goede jumpppp");
+            AddReward(1.0f);
+            EndEpisode();
+        }
+    }
+
+
+    void Update()
+    {
+        Vector3 moveDirection = Vector3.back * ObstacleSpeed * Time.deltaTime;
+        obstacleRb.MovePosition(obstacleRb.position + moveDirection);
+
+        if (obstacleRb.position.z < -9f)
+        {
+            Passed = false;
+            NegativeReceived = false;
+            ObstacleSpeed = Random.Range(4f, 7f);
+            obstacleRb.position = new Vector3(0, 0.5f, 9);
+            
+        }
+
+        if(this.transform.localPosition.y < 0){
+            EndEpisode();
+        }
+    }
+    
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        var discreteActions = actionsOut.DiscreteActions;
+        discreteActions[0] = Input.GetKey(KeyCode.Space) ? 1 : 0;
+        
+    }
+    
+}
